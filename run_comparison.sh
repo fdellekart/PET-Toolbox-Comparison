fef8f250ff0b782720ccb3ad05398a9ea2d70ff1
fdellekart <florian.dellekart@hotmail.de>
2024-10-03 10:48:28 +0200

    fix(comparison): exec does prevent rest of script to execute and fix directory setup

--- run_comparison.sh
+++ run_comparison.sh
@@ -3,24 +3,26 @@
 # This script runs the reconstruction and evaluation for all
 # specified targets and saves images and metadata to results
 
-TARGET_DIRS=(NiftyPET SIRF_STIR)
+TARGET_DIRS=(SIRF-STIR NiftyPET)
 GIT_COMMIT_SHORT_SHA=$(git rev-parse --short HEAD)
 
-printf -v date '%(%Y-%m-%d-%H-%M)T\n' -1
+printf -v date '%(%Y-%m-%d-%H-%M)T' -1
 export OUTPUT_VERSION_DIR=${date}-${GIT_COMMIT_SHORT_SHA}
+DESTINATION_PARENT_DIR=${PWD}/results/${OUTPUT_VERSION_DIR}
+mkdir "${DESTINATION_PARENT_DIR}"
 
 for target in ${TARGET_DIRS[@]}; do
-    DESTINATION_DIR=${PWD}/results/${OUTPUT_VERSION_DIR}/${target}
-    mkdir ${DESTINATION_DIR}
+    DESTINATION_DIR=${DESTINATION_PARENT_DIR}/${target}
+    mkdir "${DESTINATION_DIR}"
 
     cd $target
-    exec ./run_recon.sh
+    ./run_recon.sh
     cp ./output/result.nii.gz ${DESTINATION_DIR}/result.nii.gz
     cp ./output/metadata.json ${DESTINATION_DIR}/metadata.json
 
     cd ../image_evaluation
     cp ${DESTINATION_DIR}/result.nii.gz ./data/sub-00/pet/result.nii.gz
-    exec ./run_eval.sh
+    ./run_eval.sh
     cp ./data/pet_mni4d.nii.gz ${DESTINATION_DIR}/pet_mni4d.nii.gz
     cp ./data/result.json ${DESTINATION_DIR}/evaluation.json
 
