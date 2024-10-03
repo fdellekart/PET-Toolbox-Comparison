fix(evaluation): Run normalization and analysis in separate containers
Initially wanted to run the analysis script inside the original fsl container
to not complicate this unnecessarily. However, the FSL docker container runs
Ubuntu 14 and python 3.4 and it was not possible to upgrade this to a version
which support nibabel and nilearn. Therefore, we use an extra container with
a newer python image just for the analysis.

image_evaluation/analyze.py
@@ -4,7 +4,7 @@ import numpy as np
 import nibabel as nib
 from nilearn.datasets import fetch_atlas_aal
 
-NORM_IMAGE_FILE = "data/pet_mni4d_jsrecon.nii.gz"
+NORM_IMAGE_FILE = "data/pet_mni4d.nii.gz"
 
 results = dict()
 results["norm_file"] = NORM_IMAGE_FILE
@@ -12,7 +12,7 @@ atlas_meta = fetch_atlas_aal()
 
 atlas = nib.load(atlas_meta["maps"]).get_fdata().astype(np.int16)
 image = nib.load(NORM_IMAGE_FILE).get_fdata()
-image = image[:, :, :, 86]
+image = image[:, :, :, 0]
 image[image < 0] = 0
 
 snr_per_region = dict()
@@ -25,5 +25,5 @@ for index in map(int, atlas_meta["indices"]):
 subset = image[atlas != 0]
 results["total_snr"] = subset.mean() / subset.std()
 
-with open("results.json", "w") as f:
+with open("./data/result.json", "w") as f:
     json.dump(results, f)