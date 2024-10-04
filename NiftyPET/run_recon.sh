afbd5f5328ce3f33cfe967bb7aad796622f2193a
fdellekart <florian.dellekart@hotmail.de>
2024-10-04 16:19:58 +0200

    feat(niftypet): use build arg to only use cache until pip install from fork
    during analysis the fork is changing but docker does not know about it
    and therefore uses the build-cache. to avoid it we are using a
    build arg which changes

--- NiftyPET/run_recon.sh
+++ NiftyPET/run_recon.sh
@@ -10,10 +10,10 @@ GIT_COMMIT_SHORT_SHA=$(git rev-parse --short HEAD)
 WORKDIR=/var/work
 
 docker container rm niftypet-recon
-docker build --build-arg=GIT_COMMIT_SHORT_SHA=$GIT_COMMIT_SHORT_SHA -t niftypet-recon .
+docker build --build-arg=CACHEBUST=$(date +%s) --build-arg=GIT_COMMIT_SHORT_SHA=$GIT_COMMIT_SHORT_SHA -t niftypet-recon .
 
 echo "Running reconstruction with NiftyPET."
 echo "To see container logs run 'docker logs -t -f --since=5m niftypet-recon'."
 
-docker run -d --name=niftypet-recon --gpus=all -v ${PWD}/input:${WORKDIR}/input -v ${PWD}/output:${WORKDIR}/output niftypet-recon recon $TIME_START $TIME_END $TIME_STEP
+docker run -d --name=niftypet-recon --gpus=all -v ${PWD}/input:${WORKDIR}/input -v ${PWD}/output:${WORKDIR}/output niftypet-recon recon ${TIME_START} ${TIME_END} ${TIME_STEP}
 docker wait niftypet-recon
