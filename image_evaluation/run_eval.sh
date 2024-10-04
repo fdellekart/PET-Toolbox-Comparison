951e76ef6e967a2e5fd286f31a35e4357a59dc91
fdellekart <florian.dellekart@hotmail.de>
2024-10-04 12:34:48 +0200

    fix(evaluation): containers were not named, also remove the old container before running

--- image_evaluation/run_eval.sh
+++ image_evaluation/run_eval.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+docker container rm image-normalization
+docker container rm image-analysis
+
 docker build -f Dockerfile-normalize -t image-normalization .
 docker build -f Dockerfile-analysis -t image-analysis .
 
 echo "Normalizing images to MNI space."
-docker run -d -v ./data:/work/data image-normalization
+docker run -d --name=image-normalization -v ./data:/work/data image-normalization
 docker wait image-normalization
 
 echo "Running image quality evaluation"
-docker run -d -v ./data:/var/work/data image-analysis
+docker run -d --name=image-analysis -v ./data:/var/work/data image-analysis
 docker wait image-analysis
