9cf23a368e6227d344a6dd82e6083c1b040ed47a
fdellekart <florian.dellekart@hotmail.de>
2024-10-09 08:37:28 +0200

    Include jsrecon image into evaluation plot

--- image_eval_plot.py
+++ image_eval_plot.py
@@ -4,17 +4,20 @@ import matplotlib.pyplot as plt
 
 
 TOOLBOXES = ["NiftyPET", "SIRF-STIR"]
+datafiles = {
+    toolbox: f"results/2024-10-04-16-24-afbd5f5/{toolbox}/evaluation.json"
+    for toolbox in TOOLBOXES
+}
+datafiles["JSRecon"] = "results/JSRecon/evaluation.json"
 
 snrs = []
 
-for toolbox in TOOLBOXES:
-    datafile = f"results/2024-10-04-16-24-afbd5f5/{toolbox}/evaluation.json"
-
+for toolbox, datafile in datafiles.items():
     with open(datafile) as f:
         eval_data = json.load(f)
 
     snrs.append(list(eval_data["snr_per_region"].values()))
 
 
-plt.boxplot(snrs, tick_labels=TOOLBOXES)
+plt.boxplot(snrs, tick_labels=list(datafiles.keys()))
 plt.show()
