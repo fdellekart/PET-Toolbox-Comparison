390710e77d88ab1786970597c0a5976924c674eb
fdellekart <florian.dellekart@hotmail.de>
2024-10-17 10:51:41 +0200

    feat(errors): Check success of containers in image eval and print error message in case of error

--- image_evaluation/run_eval.sh
+++ image_evaluation/run_eval.sh
@@ -7,9 +7,21 @@ docker build -f Dockerfile-normalize -t image-normalization .
 docker build -f Dockerfile-analysis -t image-analysis .
 
 echo "Normalizing images to MNI space."
-docker run -d --name=image-normalization -v ./data:/work/data image-normalization
-docker wait image-normalization
+container_id=$(docker run -d --name=image-normalization -v ./data:/work/data image-normalization)
+status_code=$(docker wait image-normalization)
+
+if [ $status_code -ne 0 ]; then
+    echo "Error in image normalization"
+    echo "Run 'docker logs ${container_id}' to view container logs"
+    exit 1
+fi
 
 echo "Running image quality evaluation"
-docker run -d --name=image-analysis -v ./data:/var/work/data image-analysis
-docker wait image-analysis
+container_id=$(docker run -d --name=image-analysis -v ./data:/var/work/data image-analysis)
+status_code=$(docker wait image-analysis)
+
+if [ $status_code -ne 0 ]; then
+    echo "Error in image quality evaluation"
+    echo "Run 'docker logs ${container_id}' to view container logs"
+    exit 1
+fi
