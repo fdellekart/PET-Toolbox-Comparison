f2287dc6dbe5a778b01c8f2ceabe060b356cd100
fdellekart <florian.dellekart@hotmail.de>
2024-10-21 09:53:30 +0200

    feat(snrs): Change analysis script to calculate SNRs for each timeframe

--- image_evaluation/analyze.py
+++ image_evaluation/analyze.py
@@ -6,24 +6,32 @@ from nilearn.datasets import fetch_atlas_aal
 
 NORM_IMAGE_FILE = "data/pet_mni4d.nii.gz"
 
-results = dict()
-results["norm_file"] = NORM_IMAGE_FILE
+
+def get_snrs(image: np.array) -> dict:
+    results = dict()
+    snr_per_region = dict()
+    results["snr_per_region"] = snr_per_region
+
+    image[image < 0] = 0
+
+    for index in map(int, atlas_meta["indices"]):
+        subset = image[atlas == index]
+        snr_per_region[index] = subset.mean() / subset.std()
+
+    subset = image[atlas != 0]
+    results["total_snr"] = subset.mean() / subset.std()
+
+    return results
+
+
 atlas_meta = fetch_atlas_aal()
-
 atlas = nib.load(atlas_meta["maps"]).get_fdata().astype(np.int16)
-image = nib.load(NORM_IMAGE_FILE).get_fdata()
-image = image[:, :, :, 0]
-image[image < 0] = 0
+image_sequence = nib.load(NORM_IMAGE_FILE).get_fdata()
 
-snr_per_region = dict()
-results["snr_per_region"] = snr_per_region
-
-for index in map(int, atlas_meta["indices"]):
-    subset = image[atlas == index]
-    snr_per_region[index] = subset.mean() / subset.std()
-
-subset = image[atlas != 0]
-results["total_snr"] = subset.mean() / subset.std()
+results = []
+for i in range(image_sequence.shape[-1]):
+    image = image_sequence[:, :, :, i]
+    results.append(get_snrs(image))
 
 with open("./data/result.json", "w") as f:
     json.dump(results, f)
