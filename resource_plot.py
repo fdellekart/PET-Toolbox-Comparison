9ae21f6cf19c8c9bdd358bda870d34df86f5c9e1
fdellekart <florian.dellekart@hotmail.de>
2024-11-09 16:32:29 +0100

    feat(plotting): Iterate through toolboxes instead of hardcoding one

--- resource_plot.py
+++ resource_plot.py
@@ -8,29 +8,33 @@ from plotting.loading import (
 )
 from plotting.plot import plot_cpu_ram, plot_gpu, plot_disk
 
-TOOLBOX_SUPPORTS_GPU = {"SIRF-STIR": False, "NiftyPET": True}
-TOOLBOX = "SIRF-STIR"
 RUNDIR = Path("results")
-FRAME_NR = 25
 PLOT_PATH_TEMPLATE = "results/plots/{toolbox}_{resource_type}_frame{frame_nr}.png"
 get_plotpath = lambda *args: Path(
     PLOT_PATH_TEMPLATE.format(toolbox=args[0], resource_type=args[1], frame_nr=args[2])
 )
 
-resource_file = RUNDIR / TOOLBOX / "resources.csv"
-metadata_file = RUNDIR / TOOLBOX / "metadata.json"
+TOOLBOXES = {"SIRF-STIR", "NiftyPET"}
+TOOLBOX_SUPPORTS_GPU = {"SIRF-STIR": False, "NiftyPET": True}
+FRAME_NR = 25
 
-with open(metadata_file) as f:
-    metadata = json.load(f)
+for toolbox in TOOLBOXES:
+    resource_file = RUNDIR / toolbox / "resources.csv"
+    metadata_file = RUNDIR / toolbox / "metadata.json"
 
-timings = parse_timings(metadata)
+    with open(metadata_file) as f:
+        metadata = json.load(f)
 
-resource_data = parse_resources_file(resource_file)
-resource_data, frame_timings = prepare_for_single_frame_plot(
-    resource_data, timings, FRAME_NR
-)
+    timings = parse_timings(metadata)
 
-plot_disk(resource_data, frame_timings, get_plotpath(TOOLBOX, "disk", FRAME_NR))
-plot_cpu_ram(resource_data, frame_timings, get_plotpath(TOOLBOX, "cup&ram", FRAME_NR))
-if TOOLBOX_SUPPORTS_GPU[TOOLBOX]:
-    plot_gpu(resource_data, frame_timings, get_plotpath(TOOLBOX, "gpu", FRAME_NR))
+    resource_data = parse_resources_file(resource_file)
+    resource_data, frame_timings = prepare_for_single_frame_plot(
+        resource_data, timings, FRAME_NR
+    )
+
+    plot_disk(resource_data, frame_timings, get_plotpath(toolbox, "disk", FRAME_NR))
+    plot_cpu_ram(
+        resource_data, frame_timings, get_plotpath(toolbox, "cup&ram", FRAME_NR)
+    )
+    if TOOLBOX_SUPPORTS_GPU[toolbox]:
+        plot_gpu(resource_data, frame_timings, get_plotpath(toolbox, "gpu", FRAME_NR))
