9bfe7e4811efbab3472e0c824616e7edb034dbf6
fdellekart <florian.dellekart@hotmail.de>
2024-11-13 11:02:43 +0100

    feat(plotting): Run plotting script for all frames

--- resource_plot.py
+++ resource_plot.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from tqdm import tqdm
+
 from plotting.loading import (
     load_resources_and_timings,
     prepare_for_single_frame_plot,
@@ -10,27 +12,30 @@ from plotting.plot import plot_frame, plot_e7_frame
 RUNDIR = Path("results")
 TOOLBOXES = {"SIRF-STIR", "NiftyPET"}
 TOOLBOX_SUPPORTS_GPU = {"SIRF-STIR": False, "NiftyPET": True}
-FRAME_NR = 25
 
-for toolbox in TOOLBOXES:
-    timing_and_resources = load_resources_and_timings(RUNDIR / toolbox)
-    frame_timing_and_resource = prepare_for_single_frame_plot(
-        *timing_and_resources, FRAME_NR
+for frame_idx in tqdm(range(106)):
+    for toolbox in TOOLBOXES:
+        timing_and_resources = load_resources_and_timings(RUNDIR / toolbox)
+        frame_timing_and_resource = prepare_for_single_frame_plot(
+            *timing_and_resources, frame_idx
+        )
+        plot_frame(
+            *frame_timing_and_resource,
+            frame_nr=frame_idx,
+            target_dir=Path(f"./results/plots/{toolbox}"),
+            gpu=TOOLBOX_SUPPORTS_GPU[toolbox],
+        )
+
+    timing_and_resources = load_e7_resources_and_timings(
+        Path("results/JSRecon"), gpu=True
     )
-    plot_frame(
-        *frame_timing_and_resource,
-        frame_nr=FRAME_NR,
-        target_dir=Path(f"./results/plots/{toolbox}"),
-        gpu=TOOLBOX_SUPPORTS_GPU[toolbox],
+    plot_e7_frame(
+        *timing_and_resources, frame_idx, Path("./results/plots/e7-tools"), gpu=True
     )
 
-
-timing_and_resources = load_e7_resources_and_timings(Path("results/JSRecon"), gpu=True)
-plot_e7_frame(
-    *timing_and_resources, FRAME_NR, Path("./results/plots/e7-tools"), gpu=True
-)
-
-timing_and_resources = load_e7_resources_and_timings(Path("results/JSRecon"), gpu=False)
-plot_e7_frame(
-    *timing_and_resources, FRAME_NR, Path("./results/plots/e7-tools"), gpu=False
-)
+    timing_and_resources = load_e7_resources_and_timings(
+        Path("results/JSRecon"), gpu=False
+    )
+    plot_e7_frame(
+        *timing_and_resources, frame_idx, Path("./results/plots/e7-tools"), gpu=False
+    )
