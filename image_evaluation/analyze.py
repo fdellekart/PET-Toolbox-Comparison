020dbf18eab5feef23f6009ac8152b1c48ab1c6c
fdellekart <florian.dellekart@hotmail.de>
2024-11-13 14:24:59 +0100

    feat(image eval): Add calculation of CNR relative to cerbelum

--- image_evaluation/analyze.py
+++ image_evaluation/analyze.py
@@ -6,6 +6,17 @@ from nilearn.datasets import fetch_atlas_aal
 
 NORM_IMAGE_FILE = "data/pet_mni4d.nii.gz"
 
+atlas_meta = fetch_atlas_aal()
+atlas = nib.load(atlas_meta["maps"]).get_fdata().astype(np.int16)
+image_sequence = nib.load(NORM_IMAGE_FILE).get_fdata()
+
+labels = np.array(atlas_meta["labels"])
+indices = np.array(atlas_meta["indices"]).astype(np.int16)
+
+is_cerebelum = np.char.startswith(labels, "Cerebelum")
+cerebelum_ids = indices[is_cerebelum]
+other_ids = indices[~is_cerebelum]
+
 
 def get_snrs(image: np.array) -> dict:
     results = dict()
@@ -24,14 +35,28 @@ def get_snrs(image: np.array) -> dict:
     return results
 
 
-atlas_meta = fetch_atlas_aal()
-atlas = nib.load(atlas_meta["maps"]).get_fdata().astype(np.int16)
-image_sequence = nib.load(NORM_IMAGE_FILE).get_fdata()
+def get_cnrs(image: np.array) -> dict:
+    results = dict()
+    results["cnr_per_region"] = dict()
+
+    image[image < 0] = 0
+    is_cerebelum = np.isin(atlas, cerebelum_ids)
+    cerebelum_mean = image[is_cerebelum].mean()
+    cerebelum_std = image[is_cerebelum].std()
+
+    for index in map(int, other_ids):
+        mask = atlas == index
+        results["cnr_per_region"][index] = (
+            image[mask].mean() - cerebelum_mean
+        ) / cerebelum_std
+
+    return results
+
 
 results = []
 for i in range(image_sequence.shape[-1]):
     image = image_sequence[:, :, :, i]
-    results.append(get_snrs(image))
+    results.append(dict(**get_snrs(image), **get_cnrs(image)))
 
 with open("./data/result.json", "w") as f:
     json.dump(results, f)
