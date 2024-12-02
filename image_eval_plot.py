783e8e26709c9c421e87cc85d009e1f27c6e0963
fdellekart <florian.dellekart@hotmail.de>
2024-12-02 10:57:54 +0100

    Add CNR to plotting

--- image_eval_plot.py
+++ image_eval_plot.py
@@ -1,23 +1,33 @@
 import json
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
+plt.rcParams.update({"font.size": 15})
+
 TOOLBOXES = ["NiftyPET", "SIRF-STIR"]
-datafiles = {
-    toolbox: f"results/2024-10-04-16-24-afbd5f5/{toolbox}/evaluation.json"
-    for toolbox in TOOLBOXES
-}
-datafiles["JSRecon"] = "results/JSRecon/evaluation.json"
+datafiles = {toolbox: f"results/{toolbox}/evaluation.json" for toolbox in TOOLBOXES}
+datafiles["e7-tools"] = "results/JSRecon/No-GPU/evaluation.json"
 
 snrs = []
+cnrs = []
 
 for toolbox, datafile in datafiles.items():
     with open(datafile) as f:
         eval_data = json.load(f)
 
-    snrs.append(list(eval_data["snr_per_region"].values()))
+    snrs.append(list(eval_data[75]["snr_per_region"].values()))
+    cnrs.append(list(eval_data[75]["cnr_per_region"].values()))
 
-
+plt.figure(figsize=(7, 5), dpi=300)
+plt.ylabel("SNR")
 plt.boxplot(snrs, tick_labels=list(datafiles.keys()))
+plt.tight_layout()
+plt.savefig("SNR.png")
+plt.close()
+
+plt.ylabel("CNR")
+plt.boxplot(cnrs, tick_labels=list(datafiles.keys()))
+plt.tight_layout()
 plt.show()
