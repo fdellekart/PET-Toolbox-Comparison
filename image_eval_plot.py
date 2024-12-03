521081bf714203154d936fd8f44f5b746183b521
fdellekart <florian.dellekart@hotmail.de>
2024-12-03 14:57:01 +0100

    feat(plot): Add cnr over time plot

--- image_eval_plot.py
+++ image_eval_plot.py
@@ -13,14 +13,15 @@ datafiles["e7-tools"] = "results/JSRecon/No-GPU/evaluation.json"
 datafiles["SIRF"] = datafiles["SIRF-STIR"]
 datafiles.pop("SIRF-STIR")
 
-ORDER = ["SIRF", "NiftyPET", "e7-tools"]
+ORDER = ["e7-tools", "NiftyPET", "SIRF"]
 
 
 data = []
 snrs = []
 cnrs = []
 
-fig, ax = plt.subplots(1, 1, figsize=(7, 5), dpi=300)
+fig_snr, ax_snr = plt.subplots(1, 1, figsize=(7, 5), dpi=300)
+fig_cnr, ax_cnr = plt.subplots(1, 1, figsize=(7, 5), dpi=300)
 
 for toolbox in ORDER:
     datafile = datafiles[toolbox]
@@ -31,23 +32,45 @@ for toolbox in ORDER:
     snrs = np.array(
         [list(frame_vals["snr_per_region"].values()) for frame_vals in eval_data]
     )
+    cnrs = np.array(
+        [list(frame_vals["cnr_per_region"].values()) for frame_vals in eval_data]
+    )
 
-    cnrs.append(list(eval_data[75]["cnr_per_region"].values()))
+    snr_means = snrs.mean(axis=1)
+    snr_std = snrs.std(axis=1)
+    snr_upper_bound = snr_means + snr_std
+    snr_lower_bound = snr_means - snr_std
 
-    means = snrs.mean(axis=1)
-    std = snrs.std(axis=1)
-    upper_bound = means + std
-    lower_bound = means - std
+    cnr_means = cnrs.mean(axis=1)
+    cnr_means[cnr_means > 100] = 0
+    cnr_std = cnrs.std(axis=1)
+    cnr_std[cnr_std > 4] = 0
+    cnr_upper_bound = cnr_means + cnr_std
+    cnr_lower_bound = cnr_means - cnr_std
 
-    frame_nr = np.arange(len(means))
+    frame_nr = np.arange(len(snr_means))
     if toolbox == "NiftyPET":
         frame_nr += 10
 
-    ax.plot(frame_nr, means, label=toolbox)
-    ax.fill_between(frame_nr, lower_bound, upper_bound, alpha=0.2)
+    if toolbox != "SIRF":
+        ax_cnr.plot(frame_nr, cnr_means, label=toolbox)
+        ax_cnr.fill_between(frame_nr, cnr_lower_bound, cnr_upper_bound, alpha=0.2)
 
-ax.set_xlabel("Frame Nr.")
-ax.set_ylabel("SNR")
-fig.legend(loc="lower center", ncols=3, frameon=False)
-plt.tight_layout(rect=[0, 0.1, 1, 1])
-plt.savefig("image.png")
+    ax_snr.plot(frame_nr, snr_means, label=toolbox)
+    ax_snr.fill_between(frame_nr, snr_lower_bound, snr_upper_bound, alpha=0.2)
+
+ax_snr.set_xlabel("Frame Nr.")
+ax_cnr.set_xlabel("Frame Nr.")
+ax_cnr.set_ylim(0)
+
+ax_snr.set_ylabel("SNR")
+ax_cnr.set_ylabel("CNR")
+
+fig_snr.legend(loc="lower center", ncols=3, frameon=False)
+fig_cnr.legend(loc="lower center", ncols=3, frameon=False)
+
+fig_snr.tight_layout(rect=[0, 0.1, 1, 1])
+fig_cnr.tight_layout(rect=[0, 0.1, 1, 1])
+
+fig_snr.savefig("snr.png")
+fig_cnr.savefig("cnr.png")
