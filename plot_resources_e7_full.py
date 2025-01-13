63446a35b0d37947c522177a05cda012aa261379
fdellekart <florian.dellekart@hotmail.de>
2025-01-13 12:29:21 +0100

    feat(plot): Add plotting script for e7 full time

---
+++ plot_resources_e7_full.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from plotting.loading import load_e7_resources_and_timings
+
+histo_resources, _, recon_resources, _ = load_e7_resources_and_timings(
+    Path("./results/JSRecon-HighPower"), gpu=True
+)
+timediff = recon_resources.index.min() - histo_resources.index.max()
+histo_resources.index = histo_resources.index + timediff
+resources = pd.concat([histo_resources, recon_resources])
+resources.index = (resources.index - resources.index.min()).seconds / 60
+
+fig: plt.Figure
+ax_cpu: plt.Axes
+ax_mem: plt.Axes
+
+fig, (ax_cpu, ax_mem) = plt.subplots(2, 1, figsize=(7, 5), dpi=300, sharex=True)
+ax_cpu.plot(resources.index, resources["n_cpus"])
+ax_mem.plot(resources.index, resources["memory"])
+
+ax_cpu.set_ylabel("CPU util. [$n_{cores}$]")
+ax_mem.set_ylabel("Memory usage [GB]")
+ax_mem.set_xlabel("Time [min]")
+
+plt.tight_layout()
+plt.savefig("HP-GPU.png")
